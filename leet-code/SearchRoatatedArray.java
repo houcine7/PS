1e1ba665994202384911f568708dc0e2c02c0e85
Houcine EL ADDALI <eladdali.lahoucine@ine.inpt.ma>
2023-06-30 14:01:56 +0100

    search prb

--- leet-code/SearchRoatatedArray.java
+++ leet-code/SearchRoatatedArray.java
@@ -6,19 +6,7 @@ public class SearchRoatatedArray {
      */
     public int searchInRotatedArray1(int[] nums, int target) {
 
-        if (nums.length == 1) {
-            if (nums[0] == target)
-                return 0;
-            return -1;
-        }
-
-        int pivot_index = 0;
-        for (int i = 1; i < nums.length; i++) { // [1,2,3,4,5,6,7] (n-1)
-            if (nums[i] < nums[0]) {
-                pivot_index = i;
-                break;
-            }
-        }
+        int pivot_index = this.findPivot(nums, 0, nums.length);
 
         System.out.println(pivot_index);
 
@@ -56,12 +44,24 @@ public class SearchRoatatedArray {
 
     public int findPivot(int[] nums, int start, int end) {
         //
-        while (start <= end) {
+        while (start < end) {
             int mid = (end - start) / 2 + start;
-            // if()
+            System.out.println(mid);
+            if (mid < end && nums[mid + 1] < nums[mid]) {
+                return mid + 1;
+            }
+
+            if (mid > start && nums[mid] < nums[mid - 1]) {
+                return mid;
+            }
+            if (nums[mid] < nums[start]) {
+                return findPivot(nums, start, mid - 1);
+            }
+
+            return findPivot(nums, mid + 1, end);
         }
 
-        return -1;
+        return 0;
     }
 
     public static void main(String[] args) {
@@ -71,7 +71,10 @@ public class SearchRoatatedArray {
 
         SearchRoatatedArray sr = new SearchRoatatedArray();
 
-        System.out.println(sr.searchInRotatedArray1(new int[] { 4, 5, 6, 7, 0, 2, 3 }, 5));
+        // System.out.println(sr.searchInRotatedArray1(new int[] { 4, 5, 6, 7, 0, 2, 3
+        // }, 5));
+
+        System.out.println(sr.findPivot(new int[] { 1 }, 0, 1));
 
     }
 
