8f9ac86f4aee9542293c75224d4f4d083166b8dc
Houcine EL ADDALI <eladdali.lahoucine@ine.inpt.ma>
2023-07-16 21:45:33 +0100

    new commit

--- leet-code/TrappingRainWater.java
+++ leet-code/TrappingRainWater.java
@@ -1,5 +1,6 @@
 public class TrappingRainWater {
 
+    // not optimized O(n)
     public int trap(int[] height) {
 
         //
@@ -41,10 +42,37 @@ public class TrappingRainWater {
         return counter;
     }
 
+    // optimizing by precalculate the heighest on the left and on the right:
+    public int trap2(int[] height) {
+
+        int counter = 0;
+        int[] h_left = new int[height.length];
+        int[] h_right = new int[height.length];
+
+        h_left[0] = height[0];
+        for (int i = 1; i < height.length; i++) {
+            h_left[i] = Math.max(h_left[i - 1], height[i]);
+        }
+
+        h_right[height.length - 1] = height[height.length - 1];
+        for (int i = height.length - 2; i >= 0; i--) {
+            h_right[i] = Math.max(h_right[i + 1], height[i]);
+        }
+
+        for (int i = 1; i < height.length - 1; i++) {
+            int holder = Math.min(h_left[i - 1], h_right[i + 1]);
+            if (holder - height[i] > 0) {
+                counter += holder - height[i];
+            }
+        }
+
+        return counter;
+    }
+
     public static void main(String[] args) {
 
         TrappingRainWater tw = new TrappingRainWater();
-        System.out.println(tw.trap(new int[] { 3, 0, 2, 0, 4 }));
+        System.out.println(tw.trap2(new int[] { 3, 0, 2, 0, 4 }));
 
     }
 }
