9bafd45f222e9ec7d079e21012ea14e4602f1605
Houcine EL ADDALI <eladdali.lahoucine@ine.inpt.ma>
2023-08-31 13:23:46 +0100

    quick select

--- leet-code/KthLargestNumber.java
+++ leet-code/KthLargestNumber.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Random;
 
 class KthLargestNumber {
 
@@ -29,13 +33,61 @@ class KthLargestNumber {
 
     static int KthLargestNumber0(int[] nums, int k) {
 
-        return 0;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.add(num);
+        }
+
+        while (nums.length - k != 0) {
+            minHeap.poll();
+            k++;
+        }
+
+        return minHeap.poll();
+
+    }
+
+    static int KthLargestNumber2(int[] nums, int k) {
+
+        List<Integer> ls = new ArrayList<>();
+        for (int num : nums) {
+            ls.add(num);
+        }
+        return quickSelect(ls, k);
+    }
+
+    static int quickSelect(List<Integer> ls, int k) {
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> mid = new ArrayList<>();
+
+        int pivotI = new Random().nextInt(ls.size());
+        int pivot = ls.get(pivotI);
+
+        for (Integer num : ls) {
+            if (num > pivot)
+                left.add(num);
+            else if (num < pivot)
+                right.add(num);
+            else
+                mid.add(num);
+        }
+
+        if (left.size() >= k)
+            return quickSelect(left, k);
+        else if ((left.size() + mid.size()) < k)
+            return quickSelect(right, k - left.size() - mid.size());
+        else
+            return pivot;
+
     }
 
     public static void main(String[] args) {
 
         int[] a = new int[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 };
-        System.out.println(KthLargestNumber1(a, 4));
+        System.out.println(KthLargestNumber2(a, 4));
 
     }
 }
