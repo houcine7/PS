e631bc42589d5d318bd82ad01809a322d04a3c3a
Houcine EL ADDALI <eladdali.lahoucine@ine.inpt.ma>
2023-09-01 23:15:53 +0100

    quick sort

--- leet-code/MaxSlidingWindow.java
+++ leet-code/MaxSlidingWindow.java
@@ -28,8 +28,69 @@ public class MaxSlidingWindow {
         return r;
     }
 
+    static int[] maxSlidingWindow2(int[] nums, int k) {
+
+        int i = 0;
+        List<Integer> ls = new ArrayList<>();
+        while (i <= nums.length - k) {
+            int[] tep = nums.clone();
+            quickSort(tep, i, i + k - 1);
+            ls.add(tep[i + k - 1]);
+            i++;
+        }
+
+        int[] r = new int[ls.size()];
+        int m = 0;
+        for (int max : ls) {
+            r[m++] = max;
+        }
+
+        return r;
+    }
+
+    static void sortFromTo(int[] nums, int start, int end) {
+
+    }
+
+    static void quickSort(int[] nums, int start, int end) {
+        if (start < end) {
+            int pi = partition(nums, start, end);
+            quickSort(nums, start, pi - 1);
+            quickSort(nums, pi + 1, end);
+        }
+        ;
+    }
+
+    static int partition(int[] nums, int start, int end) {
+
+        // the pivot
+        int pivot = nums[end];
+        int indexMin = start - 1;
+
+        for (int i = start; i < end; i++) {
+            if (nums[i] < pivot) {
+                indexMin++;
+                swap(nums, indexMin, i);
+            }
+
+        }
+
+        swap(nums, indexMin + 1, end);
+
+        return indexMin + 1;
+    }
+
+    static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(maxSlidingWindow(new int[] { 1, 3, -1, -3, 5, 3, 6, 7 }, 3)));
+        int[] nums = new int[] { 1, 3, -1, -3, 5, 3, 6, 7 };
+
+        quickSort(nums, 0, 2);
+        System.out.println(maxSlidingWindow2(nums, 3));
     }
 
 }
